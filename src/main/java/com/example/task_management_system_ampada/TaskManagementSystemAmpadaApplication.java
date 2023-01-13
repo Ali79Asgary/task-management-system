9d20bc916aceb08d729967f2965c2c0941cd7192
Ali79Asgary <ali79asgari@gmail.com>
2023-01-14 01:10:24 +0330

    feat: initialize project.

---
+++ src/main/java/com/example/task_management_system_ampada/TaskManagementSystemAmpadaApplication.java
@@ -0,0 +1,13 @@
+package com.example.task_management_system_ampada;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskManagementSystemAmpadaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaskManagementSystemAmpadaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/task_management_system_ampada/TaskManagementSystemAmpadaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.task_management_system_ampada;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementSystemAmpadaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

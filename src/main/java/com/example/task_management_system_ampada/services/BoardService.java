9951833287c35bf8eba2dc955724584f0754da32
Ali79Asgary <ali79asgari@gmail.com>
2023-01-17 08:48:06 +0330

    feat: add BoardService and BoardServiceImpl.

---
+++ src/main/java/com/example/task_management_system_ampada/services/BoardService.java
@@ -0,0 +1,20 @@
+package com.example.task_management_system_ampada.services;
+
+import com.example.task_management_system_ampada.models.Board;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface BoardService {
+
+    Optional<Board> findBoardById(String id);
+
+    List<Board> findAllBoard();
+
+    Board saveBoard(Board board);
+
+    Board updateBoard(String id, Board newBoard);
+
+    void deleteBoardById(String id);
+
+}
---
+++ src/main/java/com/example/task_management_system_ampada/services/BoardServiceImpl.java
@@ -0,0 +1,54 @@
+package com.example.task_management_system_ampada.services;
+
+import com.example.task_management_system_ampada.models.Board;
+import com.example.task_management_system_ampada.repositories.BoardRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class BoardServiceImpl implements BoardService {
+
+    private BoardRepository boardRepository;
+
+    @Autowired
+    public BoardServiceImpl(BoardRepository boardRepository) {
+        this.boardRepository = boardRepository;
+    }
+
+    @Override
+    public Optional<Board> findBoardById(String id) {
+        return boardRepository.findById(id);
+    }
+
+    @Override
+    public List<Board> findAllBoard() {
+        return boardRepository.findAll();
+    }
+
+    @Override
+    public Board saveBoard(Board board) {
+        return boardRepository.save(board);
+    }
+
+    @Override
+    public Board updateBoard(String id, Board newBoard) {
+        return boardRepository.findById(id).map(board -> {
+            board.setBoardName(newBoard.getBoardName());
+            board.setCardsId(newBoard.getCardsId());
+            board.setCreatorId(newBoard.getCreatorId());
+            board.setModifiedOn(LocalDateTime.now());
+            return boardRepository.save(board);
+        }).orElseThrow(() -> {
+            throw new RuntimeException();
+        });
+    }
+
+    @Override
+    public void deleteBoardById(String id) {
+        boardRepository.deleteById(id);
+    }
+}

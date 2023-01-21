d9c5cfbf3f946cf7eea0af52ab05106193b044ca
ali <ali79asgari@gmail.com>
2023-01-21 05:31:16 +0330

    feat: add WebSecurity class to the project.

---
+++ src/main/java/com/example/task_management_system_ampada/security/WebSecurity.java
@@ -0,0 +1,10 @@
+package com.example.task_management_system_ampada.security;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+
+public class WebSecurity {
+
+
+}

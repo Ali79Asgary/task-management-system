2855442ab7917c2ad8279b3a5797da41e859c45f
ali <ali79asgari@gmail.com>
2023-01-23 00:07:33 +0330

    feat: add spring security dependencies to pom.xml file. add existsUserByUsername method to UserRepository.

--- src/main/java/com/example/task_management_system_ampada/repositories/UserRepository.java
+++ src/main/java/com/example/task_management_system_ampada/repositories/UserRepository.java
@@ -5,4 +5,6 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, String> {
     User findUserByUsername(String username);
+
+    boolean existsUserByUsername(String username);
 }
